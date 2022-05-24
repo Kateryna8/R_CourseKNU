79a33e4c9c28523c1c4d5e0b73701a3880921fda
Kateryna8 <80947543+Kateryna8@users.noreply.github.com>
2022-05-24 21:53:24 +0300

    Update Pmean.R

--- Pmean.R
+++ Pmean.R
@@ -1,7 +1,9 @@
+#Зчитуємо файл
 getcsv = function(directory, file_id) {
     return(read.csv(paste0(directory, "/", formatC(file_id, width = 3, flag = "0"), ".csv")))
 }
 
+#Обчислюємо середнє значення (mean) забруднення сульфатами або нітратами серед заданого переліка моніторів (id має значення за замовчуванням 1:332, ігноруємо значення NA)
 pmean = function(directory, pollutant, id = 1:332) {
     all = NULL
     for (i in id) {
@@ -10,6 +12,7 @@ pmean = function(directory, pollutant, id = 1:332) {
     return(mean(all, na.rm = TRUE))
 }
 
+#Виводимо кількість повних спостережень (nobs) для кожного файлу, дата фрейм на виході має ім'я файлу(id) та к-сть спостережень(nobs).
 complete = function(directory, id = 1:332) {
     nobs = NULL
     for (i in id) {
@@ -18,6 +21,7 @@ complete = function(directory, id = 1:332) {
     return(data.frame(id, nobs))
 }
 
+#Прописуємо умови кореляції за допомогою функції cor(), threshold за замовчуванням дорівнює 0. Функція повертає вектор значень кореляцій.
 corr = function(directory, threshold = 0) {
     corelations = numeric()
     for (i in 1:332) {
